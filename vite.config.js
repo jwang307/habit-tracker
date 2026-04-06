464b56659a295c8d00884ca47c3e11809a5aea16
John Wang <john.wang1726@gmail.com>
2026-04-05 21:13:35 -0700

    Configure GitHub Pages for my fork

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/another-habit-tracker/',
+  base: '/habit-tracker/',
   plugins: [react()],
   server: {
     watch: {
